97cd10786fb971963e44372b547d336abeb89da0
b-ivo <ivobryan12@gmail.com>
2026-09-21 06:31:19 +0200

    Simple count app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,55 +1,20 @@
 import { useState } from "react";
 import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
-import { opacity } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function HomeScreen() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-  const [greetings, setGreetings] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world</Text>
-      <Text style={styles.subtitle}>Learning React Native</Text>
-
-      <TextInput
-        style={styles.input}
-        onChangeText={(value) => setText(value)}
-        value={text}
-        placeholder="Enter your name"
-        autoFocus
-      />
-      {greetings && text !== "" && <Text>Hello {text}</Text>}
-
-      <Pressable style={styles.button} onPress={() => setCount(count + 1)}>
-        <Text style={styles.buttonText}>Press ME</Text>
+      <Text>{count}</Text>
+      <Pressable  style={styles.button} onPress={() => setCount(count+1)}> 
+        <Text style={styles.buttonText}>Increase</Text>
       </Pressable>
-      <Pressable
-        onPress={() => setGreetings(true)}
-        style={({ pressed }) => [
-          styles.button,
-          pressed && {opacity: 0.5}
-        ]}
-        disabled={text === ""}
-      >
-        <Text style={styles.buttonText}>Show Greetings</Text>
+      <Pressable style={styles.button} onPress={() => setCount(count-1)}>
+        <Text style={styles.buttonText}>Decrease</Text>
       </Pressable>
-      <Text>You pressed the button {count} times</Text>
-      <Pressable
-        onPress={() => {
-          setGreetings(false);
-          setText("");
-        }}
-        style={styles.button}
-      >
+      <Pressable style={styles.button} onPress={() => setCount(0)}>
         <Text style={styles.buttonText}>Reset</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={() => {
-        setText("");
-        setGreetings(false);
-        setCount(0)
-      }}>
-        <Text style={styles.buttonText}>Clear Name</Text>
       </Pressable>
     </View>
   );
